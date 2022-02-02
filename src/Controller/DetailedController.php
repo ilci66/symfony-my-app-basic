ccbe5af53b1dc0dbd5aceff9f3c6231bb4335d58
unknown <akbiyik.ilker7@gmail.com>
2022-02-02 11:18:53 +0100

    navigation && mimicking database request

--- src/Controller/DetailedController.php
+++ src/Controller/DetailedController.php
@@ -14,11 +14,17 @@ class DetailedController extends AbstractController
      */
     public function index($slug): Response
     {
-
+        // gotta mimick a database call here with the id 
+        // $detailedItem = "";
+        function findTheItemInfo($slug){
+            $detailedItem = array('id'=>$slug, 'name'=>$slug, 'info'=>'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse aliquet mi neque, a volutpat dui dapibus ac. Etiam elementum nec elit sit amet iaculis.');
+            return $detailedItem;
+        }
         
         return $this->render('detailed/index.html.twig', [
             'controller_name' => 'DetailedController',
-            'slug' => $slug
+            'slug' => $slug,
+            'detailed' => findTheItemInfo($slug)
         ]);
     }
 }
